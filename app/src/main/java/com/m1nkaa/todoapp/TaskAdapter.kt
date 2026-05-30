d5ad3cb94111db4c85441ef4f36d94a1afce3267
M1nkaa <polina.jasko@bk.ru>
2026-05-30 21:37:00 +0300

    feat: add delete button for each task

--- app/src/main/java/com/m1nkaa/todoapp/TaskAdapter.kt
+++ app/src/main/java/com/m1nkaa/todoapp/TaskAdapter.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskAdapter(private val tasks: MutableList<Task>) :
-    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+class TaskAdapter(
+    private val tasks: MutableList<Task>,
+    private val onDelete: () -> Unit
+) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.findViewById(R.id.textTitle)
         val checkbox: CheckBox = view.findViewById(R.id.checkboxDone)
+        val deleteButton: ImageButton = view.findViewById(R.id.buttonDelete)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -26,6 +30,15 @@ class TaskAdapter(private val tasks: MutableList<Task>) :
         val task = tasks[position]
         holder.title.text = task.title
         holder.checkbox.isChecked = task.isDone
+
+        holder.deleteButton.setOnClickListener {
+            val pos = holder.adapterPosition
+            if (pos >= 0) {
+                tasks.removeAt(pos)
+                notifyItemRemoved(pos)
+                onDelete()
+            }
+        }
     }
 
     override fun getItemCount() = tasks.size
